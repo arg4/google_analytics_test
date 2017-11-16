b0b87410233667b9db13ea80bb11b914fc2db560
arg4 <nategrimberg@gmail.com>
2017-11-16 03:01:50 -0500

    Finished work on index.php finished work on reports_gen

--- index.php
+++ index.php
@@ -1,72 +1,136 @@
-
 <!DOCTYPE HTML>  
-<html>
-<head>
-<style>
-.error {color: #FF0000;}
-</style>
-</head>
-<body>  
+<html lang="en">
+    <head>
+        <?php
+        // Require Reports Generator
+        require 'reports_generator_api.php';
+        ?>
+        <title><?php echo $CLIENT_NAME ?>-Dashboard</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    </head>
+    <body>  
+        <?php
+        // Form Processing
+        // define variables and set to empty values
+        $start_date = $end_date = "";
+        $end_dateErr = $start_dateErr = "";
+        $date_parsed_start = $date_parsed_end = "";
+        $form_err = "";
 
-<?php
-// define variables and set to empty values
-$start_date = $end_date = "";
-$end_dateErr = $start_dateErr = "";
-$date_parsed_start = $date_parsed_end = "";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Check if given start date
+            if (empty($_POST["start_date"])) {
+                $start_dateErr = "Start date is required";
+            } else {
+                $start_date = $_POST["start_date"];
+                $date_parsed_start = date_parse($start_date);
+                // Check if date complies to checks
+                $start_date = real_date($date_parsed_start);
+                if (! $start_date){
+                    $start_dateErr = "Poorly formatted end date";
+                }
+            }
+            // Check if given end date
+            if (empty($_POST["end_date"])) {
+                $end_dateErr = "End date is required";
+            } else {
+                $end_date = $_POST["end_date"];
+                $date_parsed_end = date_parse($end_date);
+                // Check if date complies to checks
+                $end_date = real_date($date_parsed_end);
+                if (! $end_date){
+                    $end_dateErr = "Poorly formatted end date";
+                }
+            }
+            // Print form error if start date or end date is off 
+            if (! ($start_date && $end_date)){
+                $form_err .= "Poorly formatted start or end date";
+            }
+        }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["start_date"])) {
-    $start_dateErr = "Start date is required";
-  } else {
-    $start_date = $_POST["start_date"];
-    $date_parsed_start = date_parse($start_date);
-    echo real_date($date_parsed_start);
-  }
-  if (empty($_POST["end_date"])) {
-    $end_dateErr = "End date is required";
-  } else {
-    $end_date = $_POST["end_date"];
-    $date_parsed_end = date_parse($end_date);
-    echo real_date($date_parsed_end);
-  }
-}
-
-function real_date( $date_arr )
-{
-    if ($date_arr["day"] && $date_arr["month"] && $date_arr["year"]) {
-        $date_str = $date_arr["year"] . "-" . $date_arr["month"] . "-" $date_arr["day"];
-        return $date_str;
-    } else {
-        return false;
-    }
-}
-?>
-
-<h2>PHP Form Validation Example</h2>
-<p><span class="error">* required field.</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-Start Date: <input type="text" name="start_date">
-  <span class="error">* <?php echo $start_dateErr;?></span>
-  <br><br>
-End Date: <input type="text" name="end_date">
-  <span class="error">* <?php echo $end_dateErr;?></span>
-  <input type="submit" name="submit" value="Submit">  
-</form>
-
-<?php
-
-// Call reports generator
-require 'reports_generator_api.php';
-
-// Return Report
-echo "<h2>Your Input:</h2>";
-echo "<br>";
-echo $start_date . "<br>" . $date_parsed_start["month"] . "<br>";
-var_dump($date_parsed_start);
-echo "<br>";
-echo $end_date . "<br>" . $date_parsed_end["month"] . "<br>";
-var_dump($date_parsed_end);
-?>
-
-</body>
+        function real_date( $date_arr )
+        {
+            if ($date_arr["day"] && $date_arr["month"] && $date_arr["year"]) {
+                $date_str = $date_arr["year"] . "-" . $date_arr["month"] . "-" . $date_arr["day"];
+                return $date_str;
+            } else {
+                return false;
+            }
+        }
+        ?>
+        <div class="container" style="margin-top:100px;">
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                <div class="form-row" >
+                    <div class="col-md-3 form-group">
+                        <input type="text" class="form-control" name="start_date" placeholder="Start Date: YYYY-MM-DD">
+                        <span class="text-danger"><?php echo $start_dateErr;?></span>
+                    </div>
+                    <div class="col-md-3 form-group">
+                        <input type="text" class="form-control" name="end_date" placeholder="End Date: YYYY-MM-DD">
+                        <span class="text-danger"><?php echo $end_dateErr;?></span>
+                    </div>
+                    <div class="col-md-1 form-group">
+                        <input class="form-control" type="submit" name="submit" value="Submit">
+                    </div>
+                </div>
+            </form>
+            <div class="row" >
+                <div class="col-md-12"><span class="text-danger"><?php echo $form_err;?></span></div>
+            </div>
+            <?php
+            // Generate A Report
+            if ($start_date && $end_date){
+                $report_obj = new Report($start_date, $end_date, $ANALYTICS, $VIEW_ID);
+                $report = $report_obj->generateReport();
+                $report_obj->getQuickReportData();
+            }
+            ?>
+            
+            <div class="row">
+                <div class="col-md-12">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Date</th>
+                                <th scope="col">Bounce Rate</th>
+                                <th scope="col">Page Views</th>
+                                <th scope="col">Adsense Revenue</th>
+                                <th scope="col">Adsense Ad Units Viewed</th>
+                                <th scope="col">Average Adsense Revenue Per Page View</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th><?php echo $start_date . "<br>" . $end_date ?></th>
+                                <td><?php
+                                    if($report_obj->quick_report){
+                                        printf("%4.5f",$report_obj->quick_report["bounce"]);
+                                    }
+                                    ?>
+                                </td>
+                                <td><?php echo $report_obj->quick_report["sessions"] ?></td>
+                                <td><?php echo $report_obj->quick_report["revenue"] ?></td>
+                                <td><?php echo $report_obj->quick_report["ads_viewed"] ?></td>
+                                <td>
+                                    <?php
+                                    if($report_obj->quick_report){
+                                        $sessions = $report_obj->quick_report["sessions"];
+                                        $revenue = $report_obj->quick_report["revenue"];
+                                        printf("$ %4.8f", $revenue / $sessions);
+                                    }
+                                    ?>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            
+        </div>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    </body>
 </html>
--- reports_generator_api.php
+++ reports_generator_api.php
@@ -1,57 +1,37 @@
 <?php
-
-$DEBUG = false;
-
-// Load the tokens file
-$realToken = '{"access_token":"ya29.Glv9BIFBH_vXmWojR30fztTZ7ywEGhuKn3uWGJwibWA54KGCuaSgDmkxDCxc8C8CPWgZKlQTdiqLZQd6jm1Y0pl_UwMYDFISDME-dLfU7lc2R9vsyx17yPHajC0C","token_type":"Bearer","refresh_token":"1\/GNfiNMLFUHXz9u0-cpYn1srDk3mJG1VJuzhbE4lpPc8qe7FGKOuSXo3fNx-y34LI","expires_in":3600,"created":1510095884}';
-
-if ($DEBUG) {
-    echo "My Token which is valid total string length: " . mb_strlen($realToken) . "<br />";
-    echo "<pre>";var_dump(json_decode($realToken));
-}
-
-// Decode the tokens file
-$json_tokens = json_decode($realToken);
-
-if ($DEBUG) {
-    var_dump($json_tokens);
-}
-
+// Load Google Libraries w/ composer
 require_once __DIR__ . '/vendor/autoload.php';
 
+// Load the tokens file
+$auth_token = '{"access_token":"ya29.Glv9BIFBH_vXmWojR30fztTZ7ywEGhuKn3uWGJwibWA54KGCuaSgDmkxDCxc8C8CPWgZKlQTdiqLZQd6jm1Y0pl_UwMYDFISDME-dLfU7lc2R9vsyx17yPHajC0C","token_type":"Bearer","refresh_token":"1\/GNfiNMLFUHXz9u0-cpYn1srDk3mJG1VJuzhbE4lpPc8qe7FGKOuSXo3fNx-y34LI","expires_in":3600,"created":1510095884}';
+
+// Decode the tokens file
+$json_tokens = json_decode($auth_token);
+
+// Start Session
 session_start();
 
+// Setup
 $client = new Google_Client();
 $client->setApplicationName('HalfHourMeals');
 $client->setClientId('451466387551-s2rqui0i3ui9f2cf0lj310go5s9m1sp7.apps.googleusercontent.com');
 $client->setClientSecret('_IVq4O_ZkB48xwWcCQaJvsqO');
 $client->setRedirectUri('urn:ietf:wg:oauth:2.0:oob');
 $client->setAccessType('offline');
-//thats a client id, not a dev key, you dont need to set it for the dev key - alan
 $client->addScope(Google_Service_Analytics::ANALYTICS_READONLY);
 
-// you also dont need to clear the old token. You can leave the old token and just check if its expired.
-
-$client->setAccessToken( $realToken /*json_encode($_tokenArray)*/ );
+$client->setAccessToken( $auth_token /*json_encode($_tokenArray)*/ );
 //check if token expired:
 if ( $client->isAccessTokenExpired() ){	
     $client->refreshToken($json_tokens->refresh_token);
     $new_access_token = $client->getAccessToken();
-
-    if ($DEBUG) {
-        echo "<pre>";
-        print_r($new_access_token);
-    }
+    $client->setAccessToken($new_access_token);
 }
 
 // Create an authorized analytics service object.
-//$analytics = new Google_Service_AnalyticsReporting($client);
-
-// Call the Analytics Reporting API V4.
-//$response = getReport($analytics);
-// var_dump($response);
-
-//printResults($response);
+$ANALYTICS = new Google_Service_AnalyticsReporting($client);
+$CLIENT_NAME = 'Half Hour Meals';
+$VIEW_ID = "80618290";
 
 
 /**
@@ -60,84 +40,99 @@ if ( $client->isAccessTokenExpired() ){
  * @param service An authorized Analytics Reporting API V4 service object.
  * @return The Analytics Reporting API V4 response.
  */
-function getReport($analytics, $start_date, $end_date) {
+class Report {
+    var $start_date;
+    var $end_date;
+    var $analytics;
+    var $view_id;
 
-  // Replace with your view ID, for example XXXX.
-  $VIEW_ID = "80618290";
+    var $full_report;
+    var $quick_report = array();
 
-  // Create the DateRange object.
-  $dateRange = new Google_Service_AnalyticsReporting_DateRange();
-  $dateRange->setStartDate("7daysAgo");
-  $dateRange->setEndDate("today");
-
-  // Create Metrics object for sessions
-  $sessions = new Google_Service_AnalyticsReporting_Metric();
-  $sessions->setExpression("ga:sessions");
-  $sessions->setAlias("sessions");
-
-  // Create Metrics object for bounce rate
-  $bounce = new Google_Service_AnalyticsReporting_Metric();
-  $bounce->setExpression("ga:bounceRate");
-  $bounce->setAlias("bounce");
-
-  // Create Metrics Object for pageviews
-  $views = new Google_Service_AnalyticsReporting_Metric();
-  $views->setExpression("ga:pageviews");
-  $views->setAlias("views");
-
-  // Create Metrics Object for adsense revenue
-  $revenue = new Google_Service_AnalyticsReporting_Metric();
-  $revenue->setExpression("ga:adsenseRevenue");
-  $revenue->setAlias("revenue");
-
-  // Create Metrics Object for 
-  $ads_viewed = new Google_Service_AnalyticsReporting_Metric();
-  $ads_viewed->setExpression("ga:adsenseAdUnitsViewed");
-  $ads_viewed->setAlias("ads_viewed");
-  
-  // Create the ReportRequest object.
-  $request = new Google_Service_AnalyticsReporting_ReportRequest();
-  $request->setViewId($VIEW_ID);
-  $request->setDateRanges($dateRange);
-  $request->setMetrics(array($sessions, $bounce, $views, $revenue, $ads_viewed));
-
-  $body = new Google_Service_AnalyticsReporting_GetReportsRequest();
-  $body->setReportRequests( array( $request) );
-  return $analytics->reports->batchGet( $body );
-}
-
-
-/**
- * Parses and prints the Analytics Reporting API V4 response.
- *
- * @param An Analytics Reporting API V4 response.
- */
-function printResults($reports) {
-  for ( $reportIndex = 0; $reportIndex < count( $reports ); $reportIndex++ ) {
-    $report = $reports[ $reportIndex ];
-    $header = $report->getColumnHeader();
-    $dimensionHeaders = $header->getDimensions();
-    $metricHeaders = $header->getMetricHeader()->getMetricHeaderEntries();
-    $rows = $report->getData()->getRows();
-
-    for ( $rowIndex = 0; $rowIndex < count($rows); $rowIndex++) {
-      $row = $rows[ $rowIndex ];
-      $dimensions = $row->getDimensions();
-      $metrics = $row->getMetrics();
-      for ($i = 0; $i < count($dimensionHeaders) && $i < count($dimensions); $i++) {
-        print($dimensionHeaders[$i] . ": " . $dimensions[$i] . "\n");
-      }
-
-      for ($j = 0; $j < count($metrics); $j++) {
-        $values = $metrics[$j]->getValues();
-        for ($k = 0; $k < count($values); $k++) {
-          $entry = $metricHeaders[$k];
-          print($entry->getName() . ": " . $values[$k] . "\n");
-        }
-      }
+    // Constructs a Report Object
+    function __construct($start, $end, $ana, $id){
+        $this->start_date = $start;
+        $this->end_date = $end;
+        $this->analytics = $ana;
+        $this->view_id = $id;
     }
-  }
+
+    // Generates A Report
+    function generateReport(){
+        // Create the DateRange object.
+        $dateRange = new Google_Service_AnalyticsReporting_DateRange();
+        $dateRange->setStartDate($this->start_date);
+        $dateRange->setEndDate($this->end_date);
+
+        // Create Metrics object for sessions
+        $sessions = new Google_Service_AnalyticsReporting_Metric();
+        $sessions->setExpression("ga:sessions");
+        $sessions->setAlias("sessions");
+
+        // Create Metrics object for bounce rate
+        $bounce = new Google_Service_AnalyticsReporting_Metric();
+        $bounce->setExpression("ga:bounceRate");
+        $bounce->setAlias("bounce");
+
+        // Create Metrics Object for pageviews
+        $views = new Google_Service_AnalyticsReporting_Metric();
+        $views->setExpression("ga:pageviews");
+        $views->setAlias("views");
+
+        // Create Metrics Object for adsense revenue
+        $revenue = new Google_Service_AnalyticsReporting_Metric();
+        $revenue->setExpression("ga:adsenseRevenue");
+        $revenue->setAlias("revenue");
+
+        // Create Metrics Object for 
+        $ads_viewed = new Google_Service_AnalyticsReporting_Metric();
+        $ads_viewed->setExpression("ga:adsenseAdUnitsViewed");
+        $ads_viewed->setAlias("ads_viewed");
+  
+        // Create the ReportRequest object.
+        $request = new Google_Service_AnalyticsReporting_ReportRequest();
+        $request->setViewId($this->view_id);
+        $request->setDateRanges($dateRange);
+        $request->setMetrics(array($sessions, $bounce, $views, $revenue, $ads_viewed));
+
+        $body = new Google_Service_AnalyticsReporting_GetReportsRequest();
+        $body->setReportRequests( array( $request) );
+        $this->full_report =  $this->analytics->reports->batchGet( $body );
+        return $this->full_report;
+    }
+    
+    // Returns the full body of the report if it exists
+    function getFullReport(){
+        if ($this->full_report){
+            return $this->full_report;
+        } else {
+            return false;
+        }
+    }
+
+    // Generates a condensed version of the report
+    function getQuickReportData(){
+        $reports = $this->full_report;
+        for ( $reportIndex = 0; $reportIndex < count( $reports ); $reportIndex++ ) {
+            $report = $reports[ $reportIndex ];
+            $header = $report->getColumnHeader();
+            $dimensionHeaders = $header->getDimensions();
+            $metricHeaders = $header->getMetricHeader()->getMetricHeaderEntries();
+            $rows = $report->getData()->getRows();
+
+            for ( $rowIndex = 0; $rowIndex < count($rows); $rowIndex++) {
+                $row = $rows[ $rowIndex ];
+                $dimensions = $row->getDimensions();
+                $metrics = $row->getMetrics();
+                
+                for ($j = 0; $j < count($metrics); $j++) {
+                    $values = $metrics[$j]->getValues();
+                    for ($k = 0; $k < count($values); $k++) {
+                        $entry = $metricHeaders[$k];
+                        $this->quick_report[$entry->getName()] = $values[$k];
+                    }
+                }
+            }
+        }
+    }
 }
-
-
-$client->setAccessToken($new_access_token);
